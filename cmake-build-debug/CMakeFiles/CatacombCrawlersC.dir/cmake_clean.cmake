fbf265c14d68e04b6626ebc9ebf468548709357d
JohnCollin <collin.g.johnson@gmail.com>
2020-07-12 12:01:23 -0400

    Add GLFW as submodule

---
+++ cmake-build-debug/CMakeFiles/CatacombCrawlersC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CatacombCrawlersC.dir/src/cpp/main.cpp.obj"
+  "CatacombCrawlersC.exe"
+  "CatacombCrawlersC.exe.manifest"
+  "CatacombCrawlersC.lib"
+  "CatacombCrawlersC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CatacombCrawlersC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
